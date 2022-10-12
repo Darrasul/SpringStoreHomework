f3fdc1e92b3dbce1be7fc5bc661ae7f07da264f8
Vitovtas Buzas <vitavtasbuzas@gmail.com>
2022-10-12 17:31:03 +0300

    init commit

---
+++ src/main/java/com/buzas/springstorehomework/SpringStoreHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.buzas.springstorehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStoreHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStoreHomeworkApplication.class, args);
+    }
+
+}
